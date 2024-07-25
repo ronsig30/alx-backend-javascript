ea5be922a3a2c4a6f792954c9dcb2ff407dd2c07
ronsig30 <ronaldrogony30@gmail.com>
2024-07-25 22:09:24 +0300

    You used to attend a place like this at some point

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+// Define and export the ClassRoom class
+export default class ClassRoom {
+  // Constructor accepts maxStudentsSize and assigns it to _maxStudentsSize
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
