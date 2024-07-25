2dffbe5a1d0d21f99e2efbd0979a913f6ffd4ecc
ronsig30 <ronaldrogony30@gmail.com>
2024-07-26 01:57:53 +0300

    Evcar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,40 +1,19 @@
 import Car from './10-car';
 
 // Define symbols for private attributes
-const brand = Symbol('brand');
-const motor = Symbol('motor');
-const color = Symbol('color');
 const range = Symbol('range');
 
 export default class EVCar extends Car {
   constructor(brandValue, motorValue, colorValue, rangeValue) {
     super(brandValue, motorValue, colorValue);
-    this[brand] = brandValue;
-    this[motor] = motorValue;
-    this[color] = colorValue;
     this[range] = rangeValue;
-  }
-
-  // Getters for the attributes
-  get brand() {
-    return this[brand];
-  }
-
-  get motor() {
-    return this[motor];
-  }
-
-  get color() {
-    return this[color];
-  }
-
-  get range() {
-    return this[range];
   }
 
   // Override the cloneCar method to return an instance of Car
   cloneCar() {
     // Create a new Car instance with the existing attributes
-    return new Car(this[brand], this[motor], this[color]);
+    return new Car(this[Car.prototype[Symbol.for('brand')]],
+      this[Car.prototype[Symbol.for('motor')]],
+      this[Car.prototype[Symbol.for('color')]]);
   }
 }
