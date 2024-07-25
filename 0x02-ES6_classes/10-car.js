8aa28046433ae761bd83113489f4a331fdb7ad87
ronsig30 <ronaldrogony30@gmail.com>
2024-07-26 00:34:51 +0300

    Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -4,24 +4,38 @@ const _color = Symbol('color');
 
 export default class Car {
   constructor(brand, motor, color) {
+    // Initialize attributes with symbols
     this[_brand] = brand;
     this[_motor] = motor;
     this[_color] = color;
   }
 
+  // Getter for brand
   get brand() {
     return this[_brand];
   }
 
+  // Getter for motor
   get motor() {
     return this[_motor];
   }
 
+  // Getter for color
   get color() {
     return this[_color];
   }
 
+  // Method to clone the car instance
   cloneCar() {
-    return new this.constructor(this[_brand], this[_motor], this[_color]);
+    // Create a new instance of the same class with private attributes set to undefined
+    const ClonedCar = Object.getPrototypeOf(this).constructor;
+    const clone = new ClonedCar(undefined, undefined, undefined);
+    
+    // Copy the symbols from the original instance to the clone
+    Object.getOwnPropertySymbols(this).forEach(symbol => {
+      clone[symbol] = this[symbol];
+    });
+
+    return clone;
   }
 }
