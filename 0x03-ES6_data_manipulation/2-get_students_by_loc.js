7153cc0ec313fb5d41d9b96e75e26d47e3fc7a95
ronsig30 <ronaldrogony30@gmail.com>
2024-07-30 01:18:59 +0300

    Filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+function getStudentsByLocation(students, city) {
+  // Check if the input is an array and city is a string
+  if (!Array.isArray(students) || typeof city !== 'string') {
+    return [];
+  }
+
+  // Use filter to get students located in the specified city
+  return students.filter((student) => student.location === city);
+}
+
+export default getStudentsByLocation;
