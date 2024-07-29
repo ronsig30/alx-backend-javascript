e32586e8732190048412bf2dc6d3e9d9238bc2b2
ronsig30 <ronaldrogony30@gmail.com>
2024-07-30 01:21:54 +0300

    Reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,11 @@
+function getStudentIdsSum(students) {
+  // Check if the input is an array
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+
+  // Use reduce to sum up all the student ids
+  return students.reduce((sum, student) => sum + student.id, 0);
+}
+
+export default getStudentIdsSum;
