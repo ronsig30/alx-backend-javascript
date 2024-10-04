1ae67470e8b29294d53e7b93052004020c068ed6
ronsig30 <ronaldrogony30@gmail.com>
2024-10-04 10:39:36 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,22 @@
-import assert from 'assert';
-import calculateNumber from './0-calcul.js';
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', () => {
-  it('should return 4 when inputs are 1 and 3', () => {
+describe('calculateNumber', function () {
+  it('should return 4 when inputs are 1 and 3', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('should return 5 when inputs are 1 and 3.7', () => {
+  it('should return 5 when inputs are 1 and 3.7', function () {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it('should return 5 when inputs are 1.2 and 3.7', () => {
+  it('should return 5 when inputs are 1.2 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should return 6 when inputs are 1.5 and 3.7', () => {
+  it('should return 6 when inputs are 1.5 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 
-  it('should return 0 when both inputs are 0', () => {
-    assert.strictEqual(calculateNumber(0, 0), 0);
-  });
+  // Additional edge case tests can be added here
 });
