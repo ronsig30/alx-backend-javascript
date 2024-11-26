a04a91478fb145e469eba0242483221ebb3cc21b
ronsig30 <ronaldrogony30@gmail.com>
2024-11-26 14:45:09 +0300

    more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -12,16 +12,12 @@ const countStudents = async (path) => {
     const headers = lines.shift().split(',');
     const students = lines.map((line) => {
       const values = line.split(',');
-      return headers.reduce((obj, header, idx) => {
-        obj[header] = values[idx];
-        return obj;
-      }, {});
+      return headers.reduce((obj, header, idx) => ({ ...obj, [header]: values[idx] }), {});
     });
 
-    const fields = students.reduce((acc, student) => {
-      const field = student.field;
+    const fields = students.reduce((acc, { firstname, field }) => {
       if (!acc[field]) acc[field] = [];
-      acc[field].push(student.firstname);
+      acc[field].push(firstname);
       return acc;
     }, {});
 
@@ -49,7 +45,7 @@ const app = http.createServer(async (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.write('This is the list of our students\n');
     try {
-      const dbPath = process.argv[2];
+      const [,, dbPath] = process.argv; // Destructure `process.argv` to extract the database path
       if (!dbPath) throw new Error('Database path not provided');
       const studentData = await countStudents(dbPath);
       res.end(studentData);
