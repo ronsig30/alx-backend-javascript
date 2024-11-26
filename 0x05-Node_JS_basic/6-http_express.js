5932a372c51506017e81809cd5e6623470929bf4
ronsig30 <ronaldrogony30@gmail.com>
2024-11-26 15:13:47 +0300

    small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,14 +3,11 @@ const express = require('express');
 
 const app = express();
 
+app.listen = 1245;
+
 // Define a route for the root endpoint
 app.get('/', (req, res) => {
   res.status(200).send('Hello Holberton School!');
-});
-
-// Listen on port 1245
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
 });
 
 // Export the app for testing or reuse
