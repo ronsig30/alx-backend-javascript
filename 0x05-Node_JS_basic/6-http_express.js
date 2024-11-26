fcd01082d057fa5699b15e988db37fd30ae17da4
ronsig30 <ronaldrogony30@gmail.com>
2024-11-26 18:48:20 +0300

    small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,11 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
 
 const app = express();
 
-app.listen = 1245;
-
-// Define a route for the root endpoint
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
-// Export the app for testing or reuse
-export default app;
+app.listen(1245);
+
+module.exports = app;
