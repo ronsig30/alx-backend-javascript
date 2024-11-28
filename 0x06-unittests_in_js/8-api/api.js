41dbc9f5938bb2ed3fa84390508c2d7f3f69768e
ronsig30 <ronaldrogony30@gmail.com>
2024-11-28 16:07:43 +0300

    Basic intergration testing

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 7865;
+const PORT = 7865;
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the payment system');
 });
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
 });
 
-module.exports = server;
+module.exports = app;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,29 +1,29 @@
 const request = require('request');
 const { expect } = require('chai');
-let server;
+
+const BASE_URL = 'http://localhost:7865';
 
 describe('Index page', () => {
-  const BASE_URL = `http://localhost:${process.env.PORT || 7865}`;
+  let server;
 
   before((done) => {
     delete require.cache[require.resolve('./api')]; // Clear the cache to reload the module
-    server = require('./api');
-    done();
+    server = require('./api').listen(7865, done);
   });
 
   after((done) => {
-    server.close(() => done());
+    server.close(done);
   });
 
   it('should return status 200 for GET /', (done) => {
-    request(`${BASE_URL}/`, (error, response, body) => {
+    request(BASE_URL, (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('should return the correct message for GET /', (done) => {
-    request(`${BASE_URL}/`, (error, response, body) => {
+    request(BASE_URL, (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
